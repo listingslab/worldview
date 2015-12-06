a682481eac8ff60a96f14840f73e8993899cef78
listingslab <listingslab@gmail.com>
2015-12-06 00:24:20 +0000

    mongoConnectStr

--- server/api/api_router.js
+++ server/api/api_router.js
@@ -10,7 +10,7 @@ var mongoConnectStr = 'mongodb://localhost/worldview';
 
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  mongoConnectStr = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
