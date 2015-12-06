73a14462476d95a6d94e6b668815a7b925ca6da2
listingslab <listingslab@gmail.com>
2015-12-06 20:34:49 +0000

    last try

--- server/app.js
+++ server/app.js
@@ -5,9 +5,7 @@ var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 var connection_string = '127.0.0.1:27017/worldview';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
 }
