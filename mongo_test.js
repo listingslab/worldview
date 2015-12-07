d49490911d9af7d3da29b26e5e15f704f35c0a3a
listingslab <listingslab@gmail.com>
2015-12-07 19:08:13 +0000

    show connection string

--- mongo_test.js
+++ mongo_test.js
@@ -1,17 +1,24 @@
 
-
-console.log('Openshift MongoDB Test');
-
-//create a very simple server using environment variables
-
 var express        	= require('express');
 var app            	= express();
+var mongoose		= require('mongoose');
+
+var mongoConnectStr = 'mongodb://localhost/worldview';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  mongoConnectStr = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
 
 
 app.get('*', function(req, res){
-	console.log('Send response3 back on request');
-	res.send('here is a response');
+	console.log('Send response back on request');
+	res.send('mongoConnectStr: ' + mongoConnectStr);
 });
+
 
 var port			= process.env.PORT || 8080;
 var ip				= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
