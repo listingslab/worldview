ca8e8c61f6d558efadc87758752adaf01042dcd7
listingslab <listingslab@gmail.com>
2015-12-13 09:36:35 +0000

    yo anglar out of the box

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name worldviewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the worldviewApp
+ */
+angular.module('worldviewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name worldviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the worldviewApp
+ */
+angular.module('worldviewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
